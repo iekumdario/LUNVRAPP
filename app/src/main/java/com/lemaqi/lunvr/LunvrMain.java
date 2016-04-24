6da46b522cc46ef92c5af96ce675f1d30d872830
damato <dani3lmt@gmail.com>
2016-04-24 02:02:00 -0500

    Adding positioning feature

--- app/src/main/java/com/lemaqi/lunvr/LunvrMain.java
+++ app/src/main/java/com/lemaqi/lunvr/LunvrMain.java
@@ -1,163 +1,284 @@
 package com.lemaqi.lunvr;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.webkit.WebView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.lemaqi.lunvr.integrations.LunvrIntegrations;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class LunvrMain extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+public class LunvrMain extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener, SensorEventListener {
+    private GoogleApiClient googleApliClient;
+    private WebView lunvrView;
+    private Location location;
+    private LunvrIntegrations lunvrIntegrations;
+    private LocationRequest locationRequest;
+    private static final int REQUEST_LOCATION = 2;
+    private static final int REQUEST_CHECK_SETTINGS = 3;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
+    private boolean azimothLoaded = false;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+    View decorView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Create an instance of GoogleAPIClient.
+        if (googleApliClient == null) {
+            googleApliClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
         setContentView(R.layout.activity_lunvr_main);
-
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
-        mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        lunvrView = (WebView) findViewById(R.id.lunvrview);
+        lunvrView.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                toggle();
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    lunvrView.reload();
+                    return true;
+                }
+                return false;
             }
         });
+        lunvrView.getSettings().setJavaScriptEnabled(true);
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        lunvrIntegrations = new LunvrIntegrations(this);
+        lunvrView.addJavascriptInterface(lunvrIntegrations, "Android");
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    private void loadVr() {
+        if (location != null) {
+            lunvrView.loadUrl("http://10.1.13.222/lunvr/");
+        }
+        //lunvrView.loadUrl("file:///android_asset/lunvrjs/index.html");
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+    protected void onStart() {
+        googleApliClient.connect();
+        super.onStart();
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
+    @Override
+    protected void onPause() {
+        stopLocationUpdates();
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApliClient.disconnect();
+        int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+        decorView.setSystemUiVisibility(uiOptions);
+        super.onStop();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == REQUEST_CHECK_SETTINGS) {
+            createLocationRequest();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                loadLocation();
+            } else {
+                // Permission was denied or request was cancelled
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
         } else {
-            show();
+            createLocationRequest();
         }
     }
 
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+    @SuppressWarnings("MissingPermission")
+    private void loadLocation() {
+        location = LocationServices.FusedLocationApi.getLastLocation(googleApliClient);
+        if (location != null) {
+            lunvrIntegrations.setLocation(location);
+            loadVr();
+        } else {
+            startLocationUpdates();
         }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    protected void startLocationUpdates() {
+        //noinspection MissingPermission
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                googleApliClient, locationRequest, this);
     }
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    protected void stopLocationUpdates() {
+        if (googleApliClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    googleApliClient, this);
+        }
+    }
+
+    protected void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApliClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+                final Status status = locationSettingsResult.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        loadLocation();
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(
+                                    LunvrMain.this,
+                                    REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(LunvrMain.this);
+                        dialog.setMessage("Sorry, I can not get a location setting available, the app will close.");
+                        dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                LunvrMain.this.finishAffinity();
+                            }
+                        });
+                        dialog.create().show();
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        stopLocationUpdates();
+        loadLocation();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mAccelerometer) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor == mMagnetometer) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+
+            lunvrIntegrations.setNorth(mCurrentDegree);
+            mCurrentDegree = -azimuthInDegress;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
+
---
+++ app/src/main/java/com/lemaqi/lunvr/integrations/LunvrIntegrations.java
@@ -0,0 +1,134 @@
+package com.lemaqi.lunvr.integrations;
+
+import android.content.Context;
+import android.location.Location;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+
+/**
+ * Created by Melly on 04/23/2016.
+ */
+public class LunvrIntegrations {
+    Context lunvrContext;
+    Location location;
+    float north;
+
+    public LunvrIntegrations(Context lunvrContext) {
+        this.lunvrContext = lunvrContext;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    @JavascriptInterface
+    public String getGeoposition() {
+        JSONObject locationObject = new JSONObject();
+        try {
+            locationObject.put("lat", location.getLatitude());
+            locationObject.put("lon", location.getLongitude());
+            //geoIntegration.setLocation(locationObject.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return locationObject.toString();
+        }
+    }
+
+    public void setNorth(float northDegree) {
+        this.north = (float)(Math.toRadians(northDegree));
+    }
+
+    @JavascriptInterface
+    public float getNorth() {
+        return this.north;
+    }
+
+    // FROM: http://raingod.com/raingod/resources/Programming/Java/Software/Moon/javafiles/MoonCalculation.java
+    // day_year - gives the day of the year for the first day of each
+    // month -1. i.e. 1st January is the 0th day of the year, 1st
+    // February is the 31st etc. Used by 'moonPhase'.
+
+    private static final int    day_year[] = { -1, -1, 30, 58, 89, 119,
+            150, 180, 211, 241, 272,
+            303, 333 };
+
+    // moon_phase_name - the English name for the different phases.
+    // Change this if you need to localise the software.
+
+    private static final String moon_phase_name[] = { "New",
+            "Waxing crescent",
+            "First quarter",
+            "Waxing gibbous",
+            "Full",
+            "Waning gibbous",
+            "Third quarter",
+            "Waning crescent" };
+
+    @JavascriptInterface
+    public String  getMoonPhase() {
+        Calendar calander = Calendar.getInstance();
+        int day = calander.get(Calendar.DAY_OF_MONTH);
+        int month = calander.get(Calendar.MONTH) + 1;
+        int year = calander.get(Calendar.YEAR);
+
+        int             phase;          // Moon phase
+        int             cent;           // Century number (1979 = 20)
+        int             epact;          // Age of the moon on Jan. 1
+        int             diy;            // Day in the year
+        int             golden;         // Moon's golden number
+
+        if (month < 0 || month > 12) month = 0;     // Just in case
+        diy = day + day_year[month];                // Day in the year
+        if ((month > 2) && this.isLeapYearP(year))
+            diy++;                                  // Leapyear fixup
+        cent = (year / 100) + 1;                    // Century number
+        golden = (year % 19) + 1;                   // Golden number
+        epact = ((11 * golden) + 20                 // Golden number
+                + (((8 * cent) + 5) / 25) - 5       // 400 year cycle
+                - (((3 * cent) / 4) - 12)) % 30;    //Leap year correction
+        if (epact <= 0)
+            epact += 30;                        // Age range is 1 .. 30
+        if ((epact == 25 && golden > 11) ||
+                epact == 24)
+            epact++;
+
+        // Calculate the phase, using the magic numbers defined above.
+        // Note that (phase and 7) is equivalent to (phase mod 8) and
+        // is needed on two days per year (when the algorithm yields 8).
+
+        phase = (((((diy + epact) * 6) + 11) % 177) / 22) & 7;
+
+        JSONObject phaseObject = new JSONObject();
+        try {
+            phaseObject.put("phase", phase);
+            phaseObject.put("phaseName", moon_phase_name[phase]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        finally {
+            return(phaseObject.toString());
+        }
+
+    }
+
+    // isLeapYearP
+    //
+    // Return true if the year is a leapyear
+
+    public  boolean isLeapYearP(int year) {
+        return ((year % 4 == 0) &&
+                ((year % 400 == 0) || (year % 100 != 0)));
+    }
+
+    @JavascriptInterface
+    public void printTest(String test) {
+        Log.i("spaceapp", "TEST: " + test);
+    }
+}
